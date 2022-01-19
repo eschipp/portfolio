40e292d5bc9047f6b8f17b8137a52d2994742135
Erin Schippers <52472490+eschipp@users.noreply.github.com>
2022-01-18 21:10:13 -0500

    centered cards in projects

--- src/components/Personal/Personal.js
+++ src/components/Personal/Personal.js
@@ -4,49 +4,49 @@ import CardGroup from 'react-bootstrap/CardGroup';
 
 const Personal = ({ onRouteChange }) => {
 	return (
-		<div>
-			<CardGroup>
-			    <Card className="card bg-transparent">
-			        <Card.Img variant="top" src="holder.js/100px160" />
-			        <Card.Body>
-			            <Card.Title>Card title</Card.Title>
-			            <Card.Text>
-	              			This card has supporting text below as a natural lead-in to additional
-			            	content.
-			        	</Card.Text>
-		        	</Card.Body>
-			        <Card.Footer>
-			        	<small className="text-muted">Last updated 3 mins ago</small>
-			        </Card.Footer>
-			    </Card>
-			    <Card className="card bg-transparent">
-			        <Card.Img variant="top" src="holder.js/100px160" />
-			        <Card.Body>
-			        	<Card.Title>Card title</Card.Title>
-			        	<Card.Text>
-			            	This card has supporting text below as a natural lead-in to additional
-			            	content.
-			        	</Card.Text>
-			        </Card.Body>
-			        <Card.Footer>
-			        	<small className="text-muted">Last updated 3 mins ago</small>
-		      		</Card.Footer>
-			    </Card>
-			    <Card className="card bg-transparent">
-			        <Card.Img variant="top" src="holder.js/100px160" />
-			        <Card.Body>
-			        	<Card.Title>Card title</Card.Title>
-			        	<Card.Text>
-			          		This card has supporting text below as a natural lead-in to additional
-			            	content.
-		        		</Card.Text>
-			        </Card.Body>
-			        <Card.Footer>
-			        	<small className="text-muted">Last updated 3 mins ago</small>
-			        </Card.Footer>
-	     		</Card>
-			</CardGroup>
-		</div>
+			<div>
+				<CardGroup>
+				    <Card className="card bg-transparent">
+				        <Card.Img variant="top" src="holder.js/100px160" />
+				        <Card.Body>
+				            <Card.Title>Card title</Card.Title>
+				            <Card.Text>
+		              			This card has supporting text below as a natural lead-in to additional
+				            	content.
+				        	</Card.Text>
+			        	</Card.Body>
+				        <Card.Footer>
+				        	<small className="text-muted">Last updated 3 mins ago</small>
+				        </Card.Footer>
+				    </Card>
+				    <Card className="card bg-transparent">
+				        <Card.Img variant="top" src="holder.js/100px160" />
+				        <Card.Body>
+				        	<Card.Title>Card title</Card.Title>
+				        	<Card.Text>
+				            	This card has supporting text below as a natural lead-in to additional
+				            	content.
+				        	</Card.Text>
+				        </Card.Body>
+				        <Card.Footer>
+				        	<small className="text-muted">Last updated 3 mins ago</small>
+			      		</Card.Footer>
+				    </Card>
+				    <Card className="card bg-transparent">
+				        <Card.Img variant="top" src="holder.js/100px160" />
+				        <Card.Body>
+				        	<Card.Title>Card title</Card.Title>
+				        	<Card.Text>
+				          		This card has supporting text below as a natural lead-in to additional
+				            	content.
+			        		</Card.Text>
+				        </Card.Body>
+				        <Card.Footer>
+				        	<small className="text-muted">Last updated 3 mins ago</small>
+				        </Card.Footer>
+		     		</Card>
+				</CardGroup>
+			</div>
 		
 		
 
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -5,53 +5,104 @@ import projecttwo from './projecttwo.JPG';
 import projectthree from './projectthree.JPG';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Stack from 'react-bootstrap/Stack';
 
 
 
 const Project = ({ onRouteChange }) => {
 	return(	
-		<CardGroup>	
-			<Card className="card bg-transparent">
-				<Card.Img variant="top" className="image" src={projectone}/>
-					<Card.Body class="cardBody">
-						<Card.Title>Face Recognition</Card.Title>
-						<Card.Text>
-							This is a wider card with supporting text below as a natural lead-in to
-							additional content. This content is a little bit longer.
-						</Card.Text>
-					</Card.Body>
-				<Card.Footer>
-					<small>View Project</small>
-				</Card.Footer>
-			</Card>
-			<Card className="card bg-transparent">
-				<Card.Img variant="top" className="image" src={projecttwo}/>
-					<Card.Body class="cardBody">
-						<Card.Title>Robofriends</Card.Title>
-						<Card.Text>
-							This is a wider card with supporting text below as a natural lead-in to
-							additional content. This content is a little bit longer.
-						</Card.Text>
-					</Card.Body>
-				<Card.Footer>
-					<small><a href="https://eschipp.github.io/robofriends/">View Project</a></small>
-				</Card.Footer>
-			</Card>
-			<Card className="card bg-transparent">
-				<Card.Img variant="top" className="image" src={projectthree}/>
-					<Card.Body class="cardBody">
-						<Card.Title>Background Generator</Card.Title>
-						<Card.Text>
-							This is a wider card with supporting text below as a natural lead-in to
-							additional content. This content is a little bit longer.
-						</Card.Text>
-					</Card.Body>
-				<Card.Footer>
-					<small><a href="https://eschipp.github.io/background-generator/">View Project</a></small>
-				</Card.Footer>
-			</Card>
-		</CardGroup>
+		<Container fluid="xxl" className="center">
+			<Row xxl={3}>
+				<Col>
+					<Card className="card bg-transparent center">
+						<Card.Img variant="top" className="image" src={projectone}/>
+							<Card.Body className="cardBody">
+								<Card.Title>Face Recognition</Card.Title>
+								<Card.Text>
+									Column 1
+								</Card.Text>
+							</Card.Body>
+						<Card.Footer>
+							<small>View Project</small>
+						</Card.Footer>
+					</Card>
+				</Col>
+				<Col>
+					<Card className="card bg-transparent center">
+						<Card.Img variant="top" className="image" src={projecttwo}/>
+							<Card.Body className="cardBody">
+								<Card.Title>Robofriends</Card.Title>
+								<Card.Text>
+								Column 2
+								</Card.Text>
+							</Card.Body>
+						<Card.Footer>
+							<small><a href="https://eschipp.github.io/robofriends/">View Project</a></small>
+						</Card.Footer>
+					</Card>
+				</Col>
+				<Col>
+					<Card className="card bg-transparent center">
+						<Card.Img variant="top" className="image" src={projectthree}/>
+							<Card.Body class="cardBody">
+								<Card.Title>Background Generator</Card.Title>
+								<Card.Text>
+									Column 3
+								</Card.Text>
+							</Card.Body>
+						<Card.Footer>
+							<small><a href="https://eschipp.github.io/background-generator/">View Project</a></small>
+						</Card.Footer>
+					</Card>
+				</Col>
+				<Col>
+					<Card className="card bg-transparent center">
+						<Card.Img variant="top" className="image" src={projectone}/>
+							<Card.Body class="cardBody">
+								<Card.Title>Face Recognition</Card.Title>
+								<Card.Text>
+									Column 4
+								</Card.Text>
+							</Card.Body>
+						<Card.Footer>
+							<small>View Project</small>
+						</Card.Footer>
+					</Card>
+				</Col>
+				<Col>
+					<Card className="card bg-transparent center">
+						<Card.Img variant="top" className="image" src={projectone}/>
+							<Card.Body class="cardBody">
+								<Card.Title>Face Recognition</Card.Title>
+								<Card.Text>
+									Column 5
+								</Card.Text>
+							</Card.Body>
+						<Card.Footer>
+							<small>View Project</small>
+						</Card.Footer>
+					</Card>
+				</Col>
+				<Col>
+					<Card className="card bg-transparent center">
+						<Card.Img variant="top" className="image" src={projectone}/>
+							<Card.Body class="cardBody">
+								<Card.Title>Face Recognition</Card.Title>
+								<Card.Text>
+									Column 6
+								</Card.Text>
+							</Card.Body>
+						<Card.Footer>
+							<small>View Project</small>
+						</Card.Footer>
+					</Card>
+				</Col>
+
+			</Row>
+		</Container>
 	)
 }
 
